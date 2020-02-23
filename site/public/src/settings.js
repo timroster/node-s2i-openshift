b0d3146bf88ca56a4440270de7aac950fd777073
Tim Robinson <timroster@gmail.com>
2020-02-23 05:35:30 -0800

    update RegEx for host only

--- site/public/src/settings.js
+++ site/public/src/settings.js
@@ -68,6 +68,7 @@ getMode();
 function validURL(str) {
   var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
     '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*))|' + // OR host name
     '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
     '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
     '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
